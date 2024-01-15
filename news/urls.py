a49f6d82a9a23e8d9c3855fab9815142c2f344d6
Hojimurod3 <147169978+Hojimurod3@users.noreply.github.com>
2024-01-15 19:11:48 +0500

    update

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page, news
+from .views import home_page, news,news2
 
 urlpatterns = [
     path('', home_page),
-    path('news/', news)
+    path('news/', news),
+    path('news2/', news2)
 ]
